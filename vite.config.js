4cc31cdeb2c3c30407e2f1cada706cfec7dd8f77
Slow-Swift <55066633+Slow-Swift@users.noreply.github.com>
2026-06-08 06:43:54 -0700

    Fixed vite config to add page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: "/fundamenta-operationum-models/"
+  base: "/fundamenta-operationum-models/",
+  
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        threejs_models: resolve(__dirname, 'threejs_models.html'),
+      }
+    }
+  }
 });
