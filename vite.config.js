5aa675143b5edc53f875f8da6f62530ffc9ca5cf
Slow-Swift <55066633+Slow-Swift@users.noreply.github.com>
2026-08-21 17:50:14 -0700

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,16 @@
 import { defineConfig } from "vite";
 import { resolve } from 'path'
 
-// export default defineConfig({
-//   base: "/fundamenta-operationum-models/",
-//
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, 'index.html'),
-//         threejs_models: resolve(__dirname, 'threejs_models.html'),
-//       }
-//     }
-//   }
-// });
-
-export library defineConfig({
+export default defineConfig({
   base: "/fundamenta-operationum-models/",
-
+  
   build: {
-    lib: {
-      entry: 'src/modelLoader.js',
-      name: 'SphericalModels',
-      filename: 'spherical-models',
-      formats: ['es']
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        threejs_models: resolve(__dirname, 'threejs_models.html'),
+      }
     }
   }
 });
+
